6560d784f8f925a6d141bfc1737f92d2477ff72d
Hans <bmm79@nau.edu>
2023-05-05 08:50:59 -0700

    Create objects.js

---
+++ 03-DataStructures/objects.js
@@ -0,0 +1,13 @@
+
+// Here are 10 practical use cases for JavaScript objects:
+
+// 1. Storing configuration settings for an application, such as API keys, URLs, and other settings.
+// 2. Representing a user's profile, including their name, email, address, and other personal information.
+// 3. Holding key-value pairs in a dictionary or a lookup table for quick access to data.
+// 4. Modeling complex hierarchical data, such as a file system, an organization chart, or a product catalog.
+// 5. Caching data to improve performance, such as memoization in recursive functions.
+// 6. Organizing and encapsulating related functions and data in a module or a namespace.
+// 7. Representing the state of a UI component, such as the current value of a form input or the checked status of a checkbox.
+// 8. Describing the structure and metadata of a JSON object for data serialization and deserialization.
+// 9. Storing temporary data in a client-side web application using the window.localStorage object.
+// 10. Using objects as a data store for simple key-value databases, such as IndexedDB or WebSQL in the browser.
